99eb9f960567827400b05e7e6c2ba7b79614ab3e
Matthew Moore <44378387+Phaysik@users.noreply.github.com>
2018-12-11 20:19:14 -0600

    :tada: :sparkles: Seperate searching js file

---
+++ JS/SearchPokemon.js
@@ -0,0 +1,49 @@
+const toTitleCase = (phrase) => {
+  return phrase.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+};
+
+function KalosSearch(){
+  $(document).ready(function() {
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val();
+
+      $(`#ids1:contains(${toTitleCase(value)})`).length > 0 ? $('#ids1').css("display", "block") : $(`#ids1:contains(${value})`).length > 0 ? $('#ids1').css("display", "block") : $('#ids1').css("display", "none");
+
+      $(`#ids2:contains(${toTitleCase(value)})`).length > 0 ? $('#ids2').css("display", "block") : $(`#ids2:contains(${value})`).length > 0 ? $('#ids2').css("display", "block") : $('#ids2').css("display", "none");
+
+      $(`#ids3:contains(${toTitleCase(value)})`).length > 0 ? $('#ids3').css("display", "block") : $(`#ids3:contains(${value})`).length > 0 ? $('#ids3').css("display", "block") : $('#ids3').css("display", "none");
+
+      $("#pokemon-container li").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+}
+
+function AlolaSearch(){
+  $(document).ready(function() {
+    $("#myInput").on("keyup", function() {
+
+      var value = $(this).val().toLowerCase();
+
+      value = (value.includes(":")) ?  value.replace(/:/, "꞉") : value;
+
+      $("#pokemon-container li").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+}
+
+function KJHSUSearch(){
+  $(document).ready(function() {
+    $("#myInput").on("keyup", function() {
+
+      var value = $(this).val().toLowerCase();
+
+      $("#pokemon-container li").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+}
