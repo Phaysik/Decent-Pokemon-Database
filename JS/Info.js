a5859b2b00229bcd3652885f7f54a9cf9eab8740
MOORE <710866@sths.ssd.k12.mo.us>
2019-03-14 14:16:16 -0500

    :tada: Index work

--- JS/Info.js
+++ JS/Info.js
@@ -4,10 +4,36 @@ const desc = [];
 const types = [];
 const ids = [];
 
-function loadPokemon() {
-  for (let i = 0; i < pokemonList.length; i++) {
-    pokemon.push(pokemonList[i].name);
-    types.push(pokemonList[i].types);
-    ids.push(pokemonList[i].nid);
+const datalist = document.getElementById('datalist');
+
+for (let i = 0; i < pokemonList.length; i++) {
+  pokemon.push(pokemonList[i].name);
+  types.push(pokemonList[i].types);
+  ids.push(pokemonList[i].nid);
+}
+
+function titleCase() {
+  $('#myInput').on('input', function(e) {
+    val = $(this).val();
+    const opts = document.getElementById('datalist').childNodes;
+    for (let i = 0; i < opts.length; i++) {
+      if (opts[i].value === toTitleCase(val)) {
+        break;
+      }
+    }
+  });
+}
+
+function indexLoad() {
+  $(document).attr('title', 'Bulbasaur');
+  datalist.textContent = '';
+  for (let i = 0; i < pokemon.length; i++) {
+    const option = document.createElement('option');
+    option.value = pokemon[i];
+    datalist.appendChild(option);
   }
+}
+
+function loadPokemon(name) {
+  $(document).attr('title', type);
 }
