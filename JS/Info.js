8f649628eb865b175729db17a0e7d98b5cc0c40b
Matthew Moore <44378387+Phaysik@users.noreply.github.com>
2019-03-23 04:24:55 -0500

    Other page pokemon load working correctly

--- JS/Info.js
+++ JS/Info.js
@@ -4,7 +4,7 @@ const desc = [];
 const types = [];
 const ids = [];
 let val = '';
-let urlPath = window.location.href.split('?');
+const urlPath = window.location.href.split('?');
 const obj = {}; // an object to store properties and values in
 let eq;
 
@@ -17,11 +17,12 @@ for (let i = 0; i < pokemonList.length; i++) {
 }
 
 window.onload = function() {
-  const title = localStorage.getItem('title'); // On reload or pageload, get the name of a Pokemon if it was loaded from another page or default to '' respectively
-  if (title !== '') {
-    loadPokemon(title);
-  } else {
-    loadPokemon();
+  if (urlPath[1]) { // if a ( ? ) was found in the split, use the second part after the ?
+    $(document).attr('title', urlPath[1]);
+    loadPokemon(urlPath[1]);
+    sessionStorage.setItem('title', urlPath[1]); // If a Pokemon was loaded from another page, then it will save it's name to the local Storage
+  } else { // If the page was not by the user clicking on a Pokemon image from another page
+    loadPokemon('Bulbasaur');
   }
 };
 
@@ -59,21 +60,5 @@ function loadDataList() {
 
 function loadPokemon(name) {
   loadDataList();
-  if (urlPath[1]) { // if a ( ? ) was found in the split, use the second part after the ?
-    urlPath = unescape(urlPath[1]).split('&'); // split urlPath into array of strings containing url variables=values
-    for (let i = 0; i < urlPath.length; i++) {
-      eq = urlPath[i].split('='); // get values from both sides of ( = ) sign
-      obj[eq[0]] = eq[1]; // insert properties and values into object
-    }
-    $(document).attr('title', Object.values(obj));
-    window.history.replaceState({index: 'index'}, Object.values(obj), 'index.html');
-  } else if (name !== 'Bulbasaur') {
-    $(document).attr('title', name);
-  } else {
-    $(document).attr('title', 'Bulbasaur');
-  }
+  $(document).attr('title', name);
 }
-
-window.onbeforeunload = function() { // Prevent use of Bulbasaur defaulting on reload
-  localStorage.setItem('title', Object.values(obj)); // If a Pokemon was loaded from another page, then it will save it's name to the local Storage
-};
