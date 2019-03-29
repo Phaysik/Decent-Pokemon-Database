bbd909f3c21fb6e0ab1e7b483ab9d890c18d0eb1
Phaysik <mmoorem0111@gmail.com>
2019-03-29 07:27:33 -0500

    :tada: Unova dynamic loading

--- JS/NationalPokemon.js
+++ JS/NationalPokemon.js
@@ -1,22 +1,22 @@
 /* eslint-disable no-unused-vars */
 let index = 0;
 const pokemonList = [];
-$.getJSON('../JSON/national.json', function (data) {
-  for (let i = 0; i < data['pokemon'].length; i++) {
-    pokemonList.push(new Pokemon(data['pokemon'][i].id, data['pokemon'][i].name, data['pokemon'][i].types));
-  }
-});
 
 window.onload = function () {
   $('#Center').text('The National Pokémon List by Pokédex Number');
   $('#myInput').val('');
   $('#pokemon-container').empty();
-  for (index; index < pokemonList.length; index++) {
-    if ($(window).scrollTop() === $(document).height() - $(window).height()) {
-      pokemonList[index].loadToPage();
-      pokemonList[index].showTypes();
-    } else {
-      break;
+  $.getJSON('../JSON/national.json', function (data) {
+    for (let i = 0; i < data['pokemon'].length; i++) {
+      pokemonList.push(new Pokemon(data['pokemon'][i].id, data['pokemon'][i].name, data['pokemon'][i].types));
     }
-  }
+    for (index; index < pokemonList.length; index++) {
+      if ($(window).scrollTop() === $(document).height() - $(window).height()) {
+        pokemonList[index].loadToPage();
+        pokemonList[index].showTypes();
+      } else {
+        break;
+      }
+    }
+  });
 };
