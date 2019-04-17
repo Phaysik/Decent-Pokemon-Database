370b5df2283765eef7f6ce0a75d8407ad99f5656
Phaysik <mmoore0111@gmail.com>
2019-04-17 14:12:09 -0500

    Some display on Index page

--- JS/Info.js
+++ JS/Info.js
@@ -32,29 +32,37 @@ window.onload = function() {
     }
     $('.appendList').empty();
 
-    if (urlPath[1]) { // if a ( ? ) was found in the split, use the second part after the ?
-      if (!pokemon.includes(toTitleCase(urlPath[1].split('=')[1].replace('%20', ' ')))) { // Redirect the user to a page not found if the query is not in the Pokemon list
-        window.location = 'set redirect location to page not found when made';
-      } else {
-        let name = toTitleCase(urlPath[1].split('=')[1]);
-        $(document).attr('title', name);
-        name = name = (name.includes('꞉')) ? name.replace(/꞉/, ':') : name;
-        listIndex = pokemon.indexOf(toTitleCase(name.replace('%20', ' ')));
-        loadPokemon(toTitleCase(name.replace('%20', ' ')));
-        JSONStat();
-        JSONEvol(types, pokemon);
-        JSONDesc();
-      }
-    } else { // If the page was not by the user clicking on a Pokemon image from another page
-      history.replaceState({index: 'index'}, '', 'Index.html?Search=Bulbasaur'); // Replace URL to make later state pushing possible
-      listIndex = 0;
-      loadPokemon('Bulbasaur');
+    determineLoad();
+  });
+};
+
+window.onpopstate = function() {
+  determineLoad();
+};
+
+function determineLoad() {
+  if (urlPath[1]) { // if a ( ? ) was found in the split, use the second part after the ?
+    if (!pokemon.includes(toTitleCase(urlPath[1].split('=')[1].replace('%20', ' ')))) { // Redirect the user to a page not found if the query is not in the Pokemon list
+      window.location = 'set redirect location to page not found when made';
+    } else {
+      let name = toTitleCase(urlPath[1].split('=')[1]);
+      $(document).attr('title', name);
+      name = name = (name.includes('꞉')) ? name.replace(/꞉/, ':') : name;
+      listIndex = pokemon.indexOf(toTitleCase(name.replace('%20', ' ')));
+      loadPokemon(toTitleCase(name.replace('%20', ' ')));
       JSONStat();
       JSONEvol(types, pokemon);
       JSONDesc();
     }
-  });
-};
+  } else { // If the page was not by the user clicking on a Pokemon image from another page
+    history.replaceState({index: 'index'}, '', 'Index.html?Search=Bulbasaur'); // Replace URL to make later state pushing possible
+    listIndex = 0;
+    loadPokemon('Bulbasaur');
+    JSONStat();
+    JSONEvol(types, pokemon);
+    JSONDesc();
+  }
+}
 
 function JSONDesc() {
   $.getJSON('../JSON/descriptions.json', function(data) {
@@ -129,18 +137,72 @@ function loadPokemon(name) {
   $(document).attr('title', name);
   $('#Center').text(name);
   name = name = (name.includes(':')) ? name.replace(/:/, '꞉') : name;
-  console.log(name);
-  $('#pokemon-info').empty().append(`<img src="../Images/Pokemon/${name}.png" alt="${name} image" width="150" height="150" />`);
+  $('#pokemon-info').empty().append(`
+    <div class="row">
+      <div class="col-5 my-auto pb-5">
+        <img src="../Images/Pokemon/${name}.png" alt="${name} image" height="300" style="max-width: 100%; display: block; margin: 0 auto;" />
+        <div data-id="${name.replace(/ /, '').replace(/:/, '').replace(/\./, '')}" class="d-flex"></div>
+      </div>
+      <div class="col-6">
+        <div>
+          <canvas id="myChart" width="400" height="400"></canvas>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-4">
+      </div>
+      <div class="col">
+      </div>
+    </div>
+  `);
 }
 
 function loadDesc(desc) {
-  $('#pokemon-info').append(`<p>${desc}</p>`);
+  $('.description').append(`<p>${desc}</p>`);
 }
 
 function loadStats(stats) {
-  for (let i = 0; i < stats.length; i++) {
-    $('#pokemon-info').append(`<p>${statNames[i]}\t${stats[i]}</p>`);
-  }
+  const ctx = document.getElementById('myChart');
+  const myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+      labels: [`HP`, `Attack`, `Defense`, `Sp. Atk`, `Sp. Def`, `Speed`, `Total`],
+      datasets: [{
+        label: 'Base Stats',
+        data: [stats[0], stats[1], stats[2], stats[3], stats[4], stats[5], stats[6]],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 130, 64, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(50, 102, 255, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(180, 180, 180, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(50, 102, 255, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(180, 180, 180, 1)',
+        ],
+        borderWidth: 1,
+      }],
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          },
+        }],
+      },
+    },
+  });
 }
 
 function loadEvolutions(evolutions, types, poke) {
@@ -156,12 +218,12 @@ function loadEvolutions(evolutions, types, poke) {
       $('#evolutions').append(`
       <div style="float: left">
         <img src="../Images/Pokemon/${evolutions[i]}.png" alt="${evolutions[i]} image" width="150" height="150" />
-        <div id="${poke[typeIndex].replace(/ /, '').replace(/:/, '').replace(/\./, '')}" class="d-flex"></div>
+        <div data-id="${poke[typeIndex].replace(/ /, '').replace(/:/, '').replace(/\./, '')}" class="d-flex"></div>
         <p>${evolutions[i]}</p>
       </div>
       `);
       for (const t of types[typeIndex]) {
-        $(`#${poke[typeIndex].replace(/ /, '').replace(/:/, '').replace(/\./, '')}`).append(`
+        $(`[data-id=${poke[typeIndex].replace(/ /, '').replace(/:/, '').replace(/\./, '')}]`).append(`
           <button class="flex-grow-1 mx-1" style="border: 1px solid black; color: ${textColors[t]}; 
           font-weight: bold; border-radius: 25px; text-align: center; ${typeColors[t]}">${t}</button>
         `);
